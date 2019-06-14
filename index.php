0930d21b069d80e7680d8e97d8f5aca9965eb2fa
sergiy <servik03@gmail.com>
2019-06-14 12:53:47 +0200

    Delete_Final

--- index.php
+++ index.php
@@ -15,6 +15,7 @@ $rows = $stmt->fetchAll();
         <td><?php echo $row['description']; ?></td>
         <td><?php echo $row['created_at']; ?></td>
         <td><a href="update.php?id=<?php echo $row['id']; ?>"><?php echo $row['id']; ?></a></td>
+        <td><a href="delete.php?id=<?php echo $row['id']; ?>">delete</a></td>
     </tr>
     <?php endforeach; ?>
 </table>
